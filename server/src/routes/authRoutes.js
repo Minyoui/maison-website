b1fd1d725ff733db54d0d711235c2d33647bfb9a
Minyoui <evanlyrush73@gmail.com>
2025-10-26 22:32:04 +0800

    23rd Commit (JWT Token)

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -2,6 +2,10 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 
+//JWT Token verification
+import verifyToken from "../middleware/authMiddleware.js";
+import jwt from "jsonwebtoken";
+
 const router = express.Router();
 
 // REGISTER ROUTE
@@ -49,11 +53,36 @@ router.post("/login", async (req, res) => {
 
         // Compare Password
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: "Invalid Credentials" });
-        }
+        if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
 
-        res.status(200).json({ message: "Login Successful!", user });
+        // Generate JWT Token
+        const token = jwt.sign (
+            { id: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" } // token will expire in 1 hour
+        );
+
+        res.status(200).json({ 
+            message: "Login Successful!",
+            token, 
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+            }, 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
+// Token Verification
+router.get("/profile", verifyToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        res.status(200).json({ message: "Profile fetched successfully", user });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Server Error" });
